f2642e3ac12cac8dff2d88a9736145b97c530379
jimbs.work <jimbs.work@gmail.com>
2025-11-09 13:19:10 +0000

    Changes before Firebase Studio auto-run

--- dist/industrial-all-packages.a53a9002.js
+++ dist/industrial-all-packages.a53a9002.js
@@ -146,7 +146,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "0.0.0.0" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44115" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34525" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
